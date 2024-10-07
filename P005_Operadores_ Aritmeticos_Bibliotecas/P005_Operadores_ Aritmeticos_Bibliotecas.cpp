816b8b67f06143b03b9dc3c9a0eade44d62ec14a
Jorge <zareksaleme14@gmail.com>
2024-10-07 08:21:28 -0600

    0000_Jorge
    Update

---
+++ P005_Operadores_ Aritmeticos_Bibliotecas/P005_Operadores_ Aritmeticos_Bibliotecas.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <math.h>
+
+int main()
+{
+
+	//Declaramos Variable
+	float numero1, numero2;
+
+	// Solicitamos al usuario que ingrese los números
+	std::cout << "Ingresa el primer número: ";
+	std::cin >> numero1;
+
+	std::cout << "Ingresa el segundo número: ";
+	std::cin >> numero2;
+
+	//Realizamos las operaciones aritméticas
+	float suma = numero1 + numero2;
+	float resta = numero1 - numero2;
+	float multiplicacion = numero1 * numero2;
+	float division = numero1 / numero2;
+	float potencia = pow(numero1, numero2);
+
+	// Mostramos los resultados
+	std::cout << "Resultados: ";
+	std::cout << "Suma: " << suma << std::endl;
+	std::cout << "Resta: " << resta << std::endl;
+	std::cout << "Multiplicación: " << multiplicacion << std::endl;
+	std::cout << "División: " << (numero2 != 0 ? division : 0) << std::endl;
+	std::cout << "Potencia (num1 ^ num2): " << potencia << std::endl;
+
+	return 0;
+}
