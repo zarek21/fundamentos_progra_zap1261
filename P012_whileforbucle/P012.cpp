a723d8eb28099660ab4a77753e653bd4626ca323
ChuckyDev <zareksaleme14@gmail.com>
2024-10-17 22:09:44 -0600

    P012
    Updtae

---
+++ P012_whileforbucle/P012.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <locale>
+#include <string>
+int main()
+{   
+//Vamos a crear un juego de vidas infinitas
+//int vidas = 3;
+std::string jugar;
+std::cout << "Bienvenido al juego de la eternidad, listo para jugar tus 3 vidas y repetir? " << std::endl;
+std::cin >> jugar;
+
+
+while (jugar == "si" || jugar == "SI") 
+{
+ for (int vidas = 3; vidas >= 0; vidas--)
+{
+    std::cout << "tus vidas son: " << vidas << std::endl;
+}
+std::cout << "Te has quedado sin vidas, volveras a vivir para volver a morir" << std::endl;
+std::cout << "¿Deseas volver a jugar? " ;
+std::cin >> jugar;
+}
+std::cout << "Ok, no volverás a jugar. Un contador del 0 al 100 empezará y terminará tu juego." << std::endl;
+
+for (int contador = 0; contador <= 100; contador++) 
+{
+    std::cout << "Contador: " << contador << std::endl;
+}
+}
