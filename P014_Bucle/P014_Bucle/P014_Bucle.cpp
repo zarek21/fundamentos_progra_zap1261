542f58057960c6c30d74e7c7bf054600db7f2930
ChuckyDev <zareksaleme14@gmail.com>
2024-10-17 22:07:50 -0600

    Ejercicios_Update
    p012
    p013
    p014

---
+++ P014_Bucle/P014_Bucle/P014_Bucle.cpp
@@ -0,0 +1,48 @@
+//Tenemos que contar del 1000 al 0 usando los 3 ciclos
+//Contar 10*10
+//Contar hasta 100 lento
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+int main()
+{
+    // Usando ciclo for
+    for (int i = 1000; i >= 0; i--) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;  
+
+    // Usando ciclo While
+    int j = 1000;
+    while (j >= 0) {
+        std::cout << j << " ";
+        j--;
+    }
+    std::cout << std::endl;  
+
+    // Usando ciclo Do While
+    int k = 1000;
+    do {
+        std::cout << k << " ";
+        k--;
+    } while (k >= 0);
+    std::cout << std::endl;  
+
+    // Contar 10x10 (hasta 100)
+    int l = 1;
+    do {
+        std::cout << l << " ";
+        l++;
+    } while (l <= 100);
+    std::cout << std::endl;  
+
+    // Contar hasta 100 lento
+    for (int m = 1; m <= 100; m++) {
+        std::cout << m << " ";
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+    std::cout << std::endl;  
+
+    return 0;
+}
